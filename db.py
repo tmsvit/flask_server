e9f3a9825cd7f68e815a14313109657d6601ea6b
Pavan Metkari <pavan.metkari02@gmail.com>
2024-03-27 10:02:01 +0530

    small chnages

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from flask_mysqldb import MySQL
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_PASSWORD'] = 'qaz$02pm'
 app.config['MYSQL_DB'] = 'tmsvit'
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/tmsvit"
